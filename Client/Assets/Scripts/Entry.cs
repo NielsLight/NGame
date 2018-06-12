26004d4e6c473ff5271d3aabf409bd689d3ad940
NielsLight <nieforever@outlook.com>
2018-06-12 18:30:37 +0800

    添加程序入口

---
+++ Client/Assets/Scripts/Entry.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using UnityEngine;
+
+namespace NGame
+{
+	public class Entry : MonoBehaviour
+	{
+
+		private  void Start()
+		{
+			try
+			{
+
+				DontDestroyOnLoad(gameObject);
+				Game.EventSystem.Add(DLLType.Model, typeof(Entry).Assembly);
+
+				Game.Scene.AddComponent<UIComponent>();
+
+				Game.EventSystem.Run(EventIdType.TestHotfixSubscribMonoEvent, "TestHotfixSubscribMonoEvent");
+			}
+			catch (Exception e)
+			{
+				Log.Error(e);
+			}
+		}
+
+		private void Update()
+		{
+			Game.EventSystem.Update();
+		}
+
+		private void LateUpdate()
+		{
+			Game.EventSystem.LateUpdate();
+		}
+
+		private void OnApplicationQuit()
+		{
+			Game.Close();
+		}
+	}
+}
