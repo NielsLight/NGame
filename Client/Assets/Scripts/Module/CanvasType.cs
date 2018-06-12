ac2bbe86fb17e8d388334dc9e6ae301dd396171c
nielslight <nielslight@gmail.com>
2018-06-12 23:47:06 +0800

    增加五行元素prefab

---
+++ Client/Assets/Scripts/Module/CanvasType.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace NGame
+{
+    public enum UICanvasType
+    {
+        /// <summary>
+        /// 五行元素的canvas
+        /// </summary>
+        GemsCanvas = 0,
+    }
+    public class CanvasType : MonoBehaviour
+    {
+        public UICanvasType uICanvasType;
+    }
+}
+
+
